8e428206b39ff23d94b1e993c466fd821e70e9fd
Mike <akramov@hostco.ru>
2018-09-16 21:41:16 +0500

    change port in server.js
    replace npm run build script from 'start' to 'postinstall'
    fix Procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const translate = require("google-translate-api");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
