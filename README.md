661f05750f5d4469be0e85bf20d9ef43f11960b7
Mike <akramov@hostco.ru>
2018-09-16 13:20:32 +0500

    add server code,
    fix css
    fix package.json startup scripts and add server dependensies

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Simple app for translate text from loaded file and saving translated text in the
 
 App worked on React.js with Axios and Express.js
 
+working at port 3000
+
 ### running
 
 npm start for starting app
@@ -11,3 +13,7 @@ npm start for starting app
 ### building
 
 npm run build for buildig app
+
+### run server
+
+npm run start-server
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,56 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
+const translate = require("google-translate-api");
+
+const port = 3000;
 const app = express();
+app.use(cors());
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
+app.use(express.static(`${__dirname}/../../build`));
+
+app.get("/", (request, response) => {
+  response.status(200).sendFile(`${__dirname}/../../build/index.html`);
 });
 
-app.listen(4000, function() {
-  console.log("Example app listening on port 3000!");
+app.get("/api/v1/translate", (request, response) => {
+  const query = request.query;
+
+  if (query.text) {
+    translate(query.text, {
+      from: query.source ? query.source : "auto",
+      to: query.target ? query.target : "en"
+    })
+      .then(res => {
+        response.status(200).send(
+          parseTranslateResults({
+            sourceText: query.text,
+            targetText: res.text
+          })
+        );
+      })
+      .catch(err => {
+        console.error({ err }, "MESSAGA: ", err.message);
+        response
+          .status(err.code ? err.code : 500)
+          .send(err.message ? err.message : "error occured.");
+      });
+  }
 });
+app.listen(port, err => {
+  if (err) {
+    return console.log("something bad happened", err);
+  }
+  console.log(`server is listening on ${port}`);
+});
+
+function parseTranslateResults({ sourceText, targetText }) {
+  const sourceArray = sourceText.split("\n"),
+    targetArray = targetText.split("\n");
+
+  const result = [];
+
+  sourceArray.forEach((sentanse, i) => {
+    result.push({ source: sentanse, target: targetArray[i] });
+  });
+  return result;
+}
